4a9bc460915243fc62444207304442b1e4a044c2
Together1314 <101439486+Together1314@users.noreply.github.com>
2022-10-01 19:47:19 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -14,7 +14,7 @@ const USER_CONFIG = {
   APP_SECRET: 'da3e95bae2c250763303dc618df2b7b1',
 
   PROVINCE: '河北',
-  CITY: '邯山区',
+  CITY: '邯郸',
 
   USERS: [
     {
@@ -29,14 +29,14 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
+          type: '*生日', name: '黄宝贝', year: '2001', date: '10-26',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '韩臭臭', year: '2001', date: '11-15',
         },
         {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
+          type: '节日', name: '恋爱纪念日', year: '2020', date: '04-25',
         },
       ],
       // 我们在一起已经有xxxx天了的配置
